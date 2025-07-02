6e614d073ebf0c1b0989423417aa234191baa3fc
Jakub20001 <96140149+Jakub20001@users.noreply.github.com>
2025-07-02 09:30:56 +0200

    Create AnnouncementForm.jsx

---
+++ Niches/Regional/Zielonki-Hub/frontend/src/components/AnnouncementForm.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import {createAnnouncement} from '../services/api'
+
+export default function AnnouncementForm({ onNew }) {
+    const [formData, setFormData] = useState({title: '', description: ''});
+
+    const handleChange = e => 
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const ann = await createAnnouncement(formData);
+        if (ann) {
+            onNew(ann);
+            setFormData({title: '', description: ''});
+
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input name="title" value={formData.title} onChange={handleChange} placeholder="Tytuł" required />
+            <textarea name="description" value={formData.description} onChange={handleChange} placeholder="Opis" required/>
+            <button type="submit">Dodaj</button>
+        </form>
+    );
+}
