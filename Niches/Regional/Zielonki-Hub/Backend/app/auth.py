ae8eaf6f458312a2307a81760545fed22710581b
Jakub20001 <96140149+Jakub20001@users.noreply.github.com>
2025-07-03 11:06:40 +0200

    Update auth.py

--- Niches/Regional/Zielonki-Hub/Backend/app/auth.py
+++ Niches/Regional/Zielonki-Hub/Backend/app/auth.py
@@ -1,3 +1,5 @@
+# Moduł uwierzytelniania: rejestracja, logowanie i generowanie tokenów JWT
+
 from datetime import datetime, timedelta, timezone
 from typing import Optional
 from jose import JWTError, jwt # type: ignore
@@ -7,27 +9,28 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from . import crud, database, models
 
+# Sekret JWT (zmień na bezpieczny!)
 SECRET_KEY = "tajnykluczJWT"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
-
+# Ustawienia haseł
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# OAuth2 dependency
 oauth2_scheme  = OAuth2PasswordBearer(tokenUrl="token")
 
-
-
+# Sprawdzenie hasła
 def verify_password(plain, hashed):
     return pwd_context.verify(plain, hashed)
-
+# Hashowanie hasła
 def get_password_hash(password):
     return pwd_context.hash(password)
-
+# Generowanie tokenu JWT
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=15))
     to_encode["exp"] = expire
     return jwt.encode(to_encode, SECRET_KEY, algorithms=[ALGORITHM])
-
+# Funkcja do pobierania aktualnego użytkownika z tokenu
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.SessionLocal)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
