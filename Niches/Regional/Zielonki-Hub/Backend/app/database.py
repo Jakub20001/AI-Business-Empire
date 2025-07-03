fb4eaa2d92746a4cdd595b08ec5f0ec32e8f1825
Jakub20001 <96140149+Jakub20001@users.noreply.github.com>
2025-07-03 11:07:31 +0200

    Update database.py

--- Niches/Regional/Zielonki-Hub/Backend/app/database.py
+++ Niches/Regional/Zielonki-Hub/Backend/app/database.py
@@ -1,3 +1,7 @@
+# Utworzenie bazy danych i sesji SQLAlchemy
+# Moduł do zarządzania bazą danych i sesjami SQLAlchemy
+# Używamy SQLite jako bazy danych, ale można to zmienić na inną
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -8,4 +12,3 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
