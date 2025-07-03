c4049101ed9b02a09d6cc4538a246e3d442eb57f
Jakub20001 <96140149+Jakub20001@users.noreply.github.com>
2025-07-03 11:07:07 +0200

    Update crud.py

--- Niches/Regional/Zielonki-Hub/Backend/app/crud.py
+++ Niches/Regional/Zielonki-Hub/Backend/app/crud.py
@@ -1,10 +1,13 @@
+# Logika aplikacji - oddzielona od endpointów (czysta warstwa operacyjna)
+
 from sqlalchemy.orm import Session
 from . import models, schemas
 from .auth import get_password_hash
 
+# Pobieranie użytkownika po nazwie użytkownika
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
-
+# Tworzenie nowego użytkownika w bazie
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_pw = get_password_hash(user.password)
     db_user = models.User(username=user.username, hashed_password=hashed_pw)
@@ -12,17 +15,13 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
-
+# Pobieranie wszystkich ogłoszeń z bazy danych
 def get_announcements(db: Session):
     return db.query(models.Announcement).all()
-
+# Tworzenie nowego ogłoszenia w bazie danych
 def create_announcement(db: Session, ann: schemas.AnnouncementCreate, user_id: int):
     db_ann = models.Announcement(**ann.dict(), owner_id=user_id)
     db.add(db_ann)
     db.commit()
     db.refresh(db_ann)
     return db_ann
-
-
-
-
